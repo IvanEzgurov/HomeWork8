4ff4c4da5a8d800a6a4b4d725d2a7e51c9f0111e
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-05-30 15:43:19 +0300

    intrf

---
+++ Interf.py
@@ -0,0 +1,9 @@
+def interface():
+    print('1 - Показать всех')
+    print('2 - Добавить контакт')
+    print('3 - Найти контакт')
+    print('4 - Удалить контакт')
+    print('5 - Изменить контакт')
+    print('6 - Выход')
+    inputNumber = input('Выберите команду: ')
+    return inputNumber
